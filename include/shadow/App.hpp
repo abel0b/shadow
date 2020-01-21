bd2d7feefb2749df9e88dab2006e65761a16fcfa
Abel Calluaud <abelcd@protonmail.com>
2020-01-21 14:40:05 +0100

    move camera using keyboard and mouse input

--- include/shadow/App.hpp
+++ include/shadow/App.hpp
@@ -13,6 +13,16 @@ private:
     Console console;
     ResourcePack resource_pack;
     Camera camera;
+    glm::vec3 position = glm::vec3(0.0, 0.0, 5.0);
+    float horizontal_angle = 3.14159f;
+    float vertical_angle = 0.0f;
+    float initialFoV = 45.0f;
+    
+    float speed = 3.0f;
+    float mouse_speed = 0.01f;
+    double current_time = glfwGetTime();
+    float last_time = current_time;
+    float delta_time = 0.0; 
 };
 
 #endif
--- src/App.cpp
+++ src/App.cpp
@@ -24,6 +24,13 @@ App::App()
     console.log("Started application");
 }
 
+
+float FoV = 45.0f;
+
+void scroll_callback(GLFWwindow * window, double xoffset, double yoffset) {
+    FoV += yoffset * 5;
+}
+
 int App::run() {
     CImg<unsigned char> image("../res/grass.png");
 
@@ -38,15 +45,15 @@ int App::run() {
     bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &terr, inputfile.c_str());
 
     if (!warn.empty()) {
-          std::cout << warn << std::endl;
+        std::cout << warn << std::endl;
     }
 
     if (!terr.empty()) {
-          std::cerr << terr << std::endl;
+        std::cerr << terr << std::endl;
     }
 
     if (!ret) {
-          exit(1);
+        exit(1);
     }
 
     // Setup window
@@ -54,7 +61,7 @@ int App::run() {
     if (!glfwInit())
         return 1;
 
-    const char* glsl_version = "#version 130";
+    const char* glsl_version = "#version 330";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
@@ -85,8 +92,8 @@ int App::run() {
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-    std::string debug_shader_name("debug");
-    resource_pack.load_shader(debug_shader_name);
+    std::string basic_shader_name("basic");
+    resource_pack.load_shader(basic_shader_name);
 
     ImVec4 clear_color = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
 
@@ -95,18 +102,97 @@ int App::run() {
     glBindVertexArray(VertexArrayID);
 
     static const GLfloat g_vertex_buffer_data[] = {
-        -1.0f, -1.0f, 0.0f,
-        1.0f, -1.0f, 0.0f,
-        0.0f,  1.0f, 0.0f,
+        -1.0f,-1.0f,-1.0f,
+        -1.0f,-1.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f,-1.0f,
+        -1.0f,-1.0f,-1.0f,
+        -1.0f, 1.0f,-1.0f,
+        1.0f,-1.0f, 1.0f,
+        -1.0f,-1.0f,-1.0f,
+        1.0f,-1.0f,-1.0f,
+        1.0f, 1.0f,-1.0f,
+        1.0f,-1.0f,-1.0f,
+        -1.0f,-1.0f,-1.0f,
+        -1.0f,-1.0f,-1.0f,
+        -1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f,-1.0f,
+        1.0f,-1.0f, 1.0f,
+        -1.0f,-1.0f, 1.0f,
+        -1.0f,-1.0f,-1.0f,
+        -1.0f, 1.0f, 1.0f,
+        -1.0f,-1.0f, 1.0f,
+        1.0f,-1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f,-1.0f,-1.0f,
+        1.0f, 1.0f,-1.0f,
+        1.0f,-1.0f,-1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f,-1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f,-1.0f,
+        -1.0f, 1.0f,-1.0f,
+        1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f,-1.0f,
+        -1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f,
+        1.0f,-1.0f, 1.0f
     };
 
+    static const GLfloat g_color_buffer_data[] = {
+        0.583f,  0.771f,  0.014f,
+        0.609f,  0.115f,  0.436f,
+        0.327f,  0.483f,  0.844f,
+        0.822f,  0.569f,  0.201f,
+        0.435f,  0.602f,  0.223f,
+        0.310f,  0.747f,  0.185f,
+        0.597f,  0.770f,  0.761f,
+        0.559f,  0.436f,  0.730f,
+        0.359f,  0.583f,  0.152f,
+        0.483f,  0.596f,  0.789f,
+        0.559f,  0.861f,  0.639f,
+        0.195f,  0.548f,  0.859f,
+        0.014f,  0.184f,  0.576f,
+        0.771f,  0.328f,  0.970f,
+        0.406f,  0.615f,  0.116f,
+        0.676f,  0.977f,  0.133f,
+        0.971f,  0.572f,  0.833f,
+        0.140f,  0.616f,  0.489f,
+        0.997f,  0.513f,  0.064f,
+        0.945f,  0.719f,  0.592f,
+        0.543f,  0.021f,  0.978f,
+        0.279f,  0.317f,  0.505f,
+        0.167f,  0.620f,  0.077f,
+        0.347f,  0.857f,  0.137f,
+        0.055f,  0.953f,  0.042f,
+        0.714f,  0.505f,  0.345f,
+        0.783f,  0.290f,  0.734f,
+        0.722f,  0.645f,  0.174f,
+        0.302f,  0.455f,  0.848f,
+        0.225f,  0.587f,  0.040f,
+        0.517f,  0.713f,  0.338f,
+        0.053f,  0.959f,  0.120f,
+        0.393f,  0.621f,  0.362f,
+        0.673f,  0.211f,  0.457f,
+        0.820f,  0.883f,  0.371f,
+        0.982f,  0.099f,  0.879f
+    };
+
+
     GLuint vertexbuffer;
-    // Generate 1 buffer, put the resulting identifier in vertexbuffer
     glGenBuffers(1, &vertexbuffer);
-    // The following commands will talk about our 'vertexbuffer' buffer
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    // Give our vertices to OpenGL.
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+    GLuint colorbuffer;
+    glGenBuffers(1, &colorbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
 
     bool settings_active = true;
     float my_color[4];
@@ -120,7 +206,7 @@ int App::run() {
         "Exponential variance shadow mapping"
     };
 
-    const char * message = "Shadow mapping algorithm";
+    const char * message = "Shadow mapping algorithms";
     
     int nb_objects = 42;
     int algorithm_selected = 0;
@@ -129,7 +215,20 @@ int App::run() {
     double z = 568.425376213;
 
     auto color = ImVec4(242.0/255.0, 214.0/255.0, 75.0/255.0, 1);
-    // Main loop
+    
+    // position
+    glfwSetScrollCallback(
+        window,
+        scroll_callback
+    );   
+
+    {
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glfwSetCursorPos(window, display_w/2, display_h/2);
+    }
+
+// Main loop
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
 
@@ -182,17 +281,72 @@ int App::run() {
 
         int display_w, display_h;
         glfwGetFramebufferSize(window, &display_w, &display_h);
+        
+        double mouse_x, mouse_y;
+        glfwGetCursorPos(window, &mouse_x, &mouse_y);
+        glfwSetCursorPos(window, display_w/2, display_h/2);
+        
+        current_time = glfwGetTime();
+        delta_time = current_time - last_time;
+        last_time = current_time;
+
+        horizontal_angle += mouse_speed * delta_time * (display_w/2.0 - mouse_x);
+        vertical_angle   += mouse_speed * delta_time * (display_h/2.0 - mouse_y);
+
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+            glfwSetWindowShouldClose(window, true);
+        }
+        glm::vec3 direction(
+            cos(vertical_angle) * sin(horizontal_angle),
+            sin(vertical_angle),
+            cos(vertical_angle) * cos(horizontal_angle)
+        );
+
+        glm::vec3 right = glm::vec3(
+            sin(horizontal_angle - 3.14159f/2.0f),
+            0,
+            cos(horizontal_angle - 3.14159f/2.0f)
+        );
+
+        glm::mat4 projection = glm::perspective(glm::radians(FoV), (float)display_w / (float)display_h, 0.1f, 100.0f);
+        glm::vec3 up = glm::cross(right, direction);
+
+        glm::mat4 view = glm::lookAt(
+            position,
+            position+direction,
+            up
+        );
+
+        if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS){
+                position += direction * delta_time * speed;
+        }
+        // Move backward
+        if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS){
+            position -= direction * delta_time * speed;
+        }
+        
+        // Strafe right
+        if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS){
+            position += right * delta_time * speed;
+        }
+        
+        // Strafe left
+        if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS){
+            position -= right * delta_time * speed;
+        }
+
         glViewport(0, 0, display_w, display_h);
         glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
-        glm::mat4 mvp = camera.get_perspective() * camera.get_view();
+        glm::mat4 mvp = projection * view;
+        // glm::mat4 mvp = camera.get_perspective() * camera.get_view();
 
-        GLuint MatrixID = glGetUniformLocation(resource_pack.get_program(debug_shader_name), "matrix");
+        GLuint MatrixID = glGetUniformLocation(resource_pack.get_program(basic_shader_name), "matrix");
 
         glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
 
-        glUseProgram(resource_pack.get_program(debug_shader_name));
+        glUseProgram(resource_pack.get_program(basic_shader_name));
 
         glEnableVertexAttribArray(0);
         glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
@@ -204,8 +358,20 @@ int App::run() {
             0,
             (void*)0
         );
+        
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+        glVertexAttribPointer(
+            1,
+            3,
+            GL_FLOAT,
+            GL_FALSE,
+            0,
+            (void*)0
+        );
+        
         // Draw the triangle !
-        glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+        glDrawArrays(GL_TRIANGLES, 0, 12*3);
         glDisableVertexAttribArray(0);
 
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
